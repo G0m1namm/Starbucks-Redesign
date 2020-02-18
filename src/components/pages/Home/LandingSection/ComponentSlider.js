dabf037cc66ac704b55f79be267409f536f9f539
G0m1namm <37153701+G0m1namm@users.noreply.github.com>
2020-02-18 12:01:35 -0500

    estilizando los controles del slider

--- src/components/pages/Home/LandingSection/ComponentSlider.js
+++ src/components/pages/Home/LandingSection/ComponentSlider.js
@@ -5,6 +5,8 @@ import ComponentSlideTwo from "./ComponentSlideTwo";
 import ComponentSlideThree from "./ComponentThree";
 import { motion, AnimatePresence } from "framer-motion";
 import { wrap } from "@popmotion/popcorn";
+import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded';
+import ArrowForwardIosRoundedIcon from '@material-ui/icons/ArrowForwardIosRounded';
 
 const components = [
     <ComponentSlideOne />,
@@ -33,6 +35,19 @@ const variants = {
     }
 };
 
+export const SliderControls = ({handleNext, handlePrev}) => {
+    return (
+        <div id="componentSliderControls">
+            <div className="slider-control-actions">
+                <button className="btn btn-primary" onClick={handlePrev}><ArrowBackIosRoundedIcon/></button>
+                <button className="btn btn-primary" onClick={handleNext}><ArrowForwardIosRoundedIcon/></button>
+            </div>
+            <div className="slider-control-info">
+
+            </div>
+        </div>
+    );
+}
 export default function ComponentSlider() {
     const [[page, direction], setPage] = useState([0, 0]);
     const imageIndex = wrap(0, components.length, page);
@@ -56,16 +71,18 @@ export default function ComponentSlider() {
                             x: { type: "spring", stiffness: 300, damping: 200 },
                             opacity: { duration: 0.2 }
                         }}
+                        className="component-wrapper-animated"
                     >
                         {components[imageIndex]}
                     </motion.div>
                 </AnimatePresence>
-            <div className="next" onClick={() => paginate(1)}>
+                {/* <div className="next" onClick={() => paginate(1)}>
                 {"Next ‣"}
             </div>
             <div className="prev" onClick={() => paginate(-1)}>
                 {"Prev ‣"}
-            </div>
+            </div> */}
+                <SliderControls handleNext={() => paginate(1)} handlePrev={() => paginate(-1)}/>
             </section>
         </>
     );
