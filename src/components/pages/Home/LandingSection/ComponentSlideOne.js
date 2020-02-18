709d8df77ffeda88ed5720df9d2dd55f9911c3b5
G0m1namm <37153701+G0m1namm@users.noreply.github.com>
2020-02-18 17:06:17 -0500

    el slider de componentes funciona en autoplay

--- src/components/pages/Home/LandingSection/ComponentSlideOne.js
+++ src/components/pages/Home/LandingSection/ComponentSlideOne.js
@@ -3,26 +3,67 @@ import './ComponentSlide.scss'
 import { ComponentCategory, ComponentTitle } from "../../../organisms/Home/ComponenSlider/ComponentInfo";
 import FrappuccinoImage from "../../../../assets/images/frappuccino-image-slide-bg.png";
 import { DescriptionSwitchView, ImageSwitchView } from "../../../molecules/Home/ComponentSlider/ComponentSwitchView";
+import { motion } from "framer-motion";
 
 export default function ComponentSlideOne() {
+    const variants = {
+        initial: {
+            y: -20,
+            opacity: 0,
+        },
+        animate: i => ({
+            y: 0,
+            opacity: 1,
+            transition: {
+                when: "beforeChildren",
+                staggerChildren: 0.1
+            }
+        }),
+        exit: {
+            y: -20,
+            opacity: 0,
+        }
+    }
+
+    const stagger = {
+        initial: { y: 20, opacity: 0, transition: {
+            staggerChildren: 0.2,
+            staggerDirection: -1,
+        }},
+        animate: {
+          y: 0,
+          opacity: 1
+        },
+        exit: {
+            y: -20,
+            opacity: 0,
+        }
+      };
+
     return (
-        <div id="componentSlideOne">
-            <div className="component-one-info">
-                <ComponentCategory>Summer drinks</ComponentCategory>
-                <ComponentTitle>Meet our new <span className="active">Summer</span> Coffeedrinks</ComponentTitle>
-                <DescriptionSwitchView
-                    desktopText="Cold latte and cappuccino, frappuccino, iced mocha are available in our new summer menu."
-                    mobileText="Exclusive deals on drinks, delivered straight to your inbox."
-                    buttonText="Find out more"
-                />
-            </div>
+        <motion.div positionTransition initial={false} variants={stagger} exit="initial" id="componentSlideOne">
+            <motion.div className="component-one-info" variants={variants} initial="initial" animate="animate" >
+                <motion.div variants={stagger} custom={1}>
+                    <ComponentCategory>Summer drinks</ComponentCategory>
+                </motion.div>
+                <motion.div variants={stagger} custom={2}>
+                    <ComponentTitle>Meet our new <span className="active">Summer</span> Coffeedrinks</ComponentTitle>
+                </motion.div>
+                <motion.div variants={stagger} custom={3}>
+                    <DescriptionSwitchView
+                        desktopText="Cold latte and cappuccino, frappuccino, iced mocha are available in our new summer menu."
+                        mobileText="Exclusive deals on drinks, delivered straight to your inbox."
+                        buttonText="Find out more"
+                    />
+                </motion.div>
+            </motion.div>
             <div className="component-one-image">
-                <ImageSwitchView 
-                    image={FrappuccinoImage} 
-                    imgAlt="Starbuck frappuccino" 
+                <ImageSwitchView
+                    image={FrappuccinoImage}
+                    imgAlt="Starbuck frappuccino"
                     buttonText="Find out more"
                 />
             </div>
-        </div>
+        </motion.div>
     );
 }
--- src/components/pages/Home/LandingSection/ComponentSlider.js
+++ src/components/pages/Home/LandingSection/ComponentSlider.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import ComponentSlideOne from "./ComponentSlideOne";
 import "./ComponentSlide.scss";
 import ComponentSlideTwo from "./ComponentSlideTwo";
@@ -8,6 +8,9 @@ import { wrap } from "@popmotion/popcorn";
 import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded';
 import ArrowForwardIosRoundedIcon from '@material-ui/icons/ArrowForwardIosRounded';
 import { LinearProgress } from "@material-ui/core";
+
+const CountStateContext = React.createContext();
+const CountDispatchContext = React.createContext();
 
 const components = [
     <ComponentSlideOne />,
@@ -36,18 +39,47 @@ const variants = {
     }
 };
 
-export const SliderControls = ({ handleNext, handlePrev }) => {
+export function LinearDeterminate({ handleNext, ...props }) {
+    const [counter, setCounter] = React.useState(0);
+    const dispatch = useContext(CountDispatchContext);
+    const reset = () => {
+        dispatch(1);
+        setCounter(0);
+    }
+    // Third Attempts
+    React.useEffect(() => {
+        const timer =
+            counter < 100 ? setInterval(() => setCounter(counter + 1), 100) : reset();
+
+        return () => clearInterval(timer);
+    }, [counter]);
+
+
+    return (
+        <LinearProgress
+            variant="determinate"
+            defaultValue={0}
+            value={counter}
+            {...props}
+        />
+    );
+}
+
+export const SliderControls = () => {
+    const page = useContext(CountStateContext);
+    const dispatch = useContext(CountDispatchContext);
+
     return (
         <div id="componentSliderControls">
             <div className="slider-control-actions">
-                <button className="btn btn-primary" onClick={handlePrev}><ArrowBackIosRoundedIcon /></button>
-                <button className="btn btn-primary" onClick={handleNext}><ArrowForwardIosRoundedIcon /></button>
+                <motion.button whileHover={{ scale: 1.03 }} whileTap={{ scale: 0.97 }} className="btn btn-primary" onClick={() => dispatch(1)}><ArrowBackIosRoundedIcon /></motion.button>
+                <motion.button whileHover={{ scale: 1.03 }} whileTap={{ scale: 0.97 }} className="btn btn-primary" onClick={() => dispatch(-1)}><ArrowForwardIosRoundedIcon /></motion.button>
             </div>
             <div className="slider-control-info">
-                <LinearProgress className="slider-progress-bar" />
+                <LinearDeterminate className="slider-progress-bar" handleNext={() => dispatch(1)} />
                 <div className="slider-control-item-indicator">
                     <span>Next slide</span>
-                    <span>03</span>
+                    <span>0{page + 1}</span>
                 </div>
                 <h3 className="slider-control-next-title">Our best coffee beans</h3>
                 <p className="slider-control-next-description">See how we roast them</p>
@@ -55,6 +87,18 @@ export const SliderControls = ({ handleNext, handlePrev }) => {
         </div>
     );
 }
+
+export const ActualSlideProgressBar = () => {
+    const page = useContext(CountStateContext);
+
+    return (
+        <div id="actualSlideTimer">
+            <span className="actual-slider-number">0{page}</span>
+            <LinearDeterminate className="actual-slider-progress-bar" />
+        </div>
+    );
+}
+
 export default function ComponentSlider() {
     const [[page, direction], setPage] = useState([0, 0]);
     const imageIndex = wrap(0, components.length, page);
@@ -63,34 +107,35 @@ export default function ComponentSlider() {
         setPage([page + newDirection, newDirection]);
     };
 
+
+
+
     return (
-        <>
-            <section id="componentSlider">
+        <section id="componentSlider">
+            <motion.div
+                key={page}
+                custom={direction}
+                variants={variants}
+                initial={"enter"}
+                animate={"center"}
+                exit={"exit"}
+                transition={{
+                    x: { type: "spring", stiffness: 300, damping: 200 },
+                    opacity: { duration: 0.2 },
+                    when: "beforeChildren"
+                }}
+                className="component-wrapper-animated"
+            >
                 <AnimatePresence initial={false} custom={direction} exitBeforeEnter>
-                    <motion.div
-                        key={page}
-                        custom={direction}
-                        variants={variants}
-                        initial={"enter"}
-                        animate={"center"}
-                        exit={"exit"}
-                        transition={{
-                            x: { type: "spring", stiffness: 300, damping: 200 },
-                            opacity: { duration: 0.2 }
-                        }}
-                        className="component-wrapper-animated"
-                    >
-                        {components[imageIndex]}
-                    </motion.div>
+                    {components[imageIndex]}
                 </AnimatePresence>
-                {/* <div className="next" onClick={() => paginate(1)}>
-                {"Next ‣"}
-            </div>
-            <div className="prev" onClick={() => paginate(-1)}>
-                {"Prev ‣"}
-            </div> */}
-                <SliderControls handleNext={() => paginate(1)} handlePrev={() => paginate(-1)} />
-            </section>
-        </>
+            </motion.div>
+            <CountStateContext.Provider value={imageIndex + 1}>
+                <CountDispatchContext.Provider value={paginate}>
+                    <ActualSlideProgressBar />
+                    <SliderControls />
+                </CountDispatchContext.Provider>
+            </CountStateContext.Provider >
+        </section>
     );
 }
