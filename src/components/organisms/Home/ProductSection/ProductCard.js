ae7a91e9fde42e617a9ce753e4a380b346cfd890
G0m1namm <37153701+G0m1namm@users.noreply.github.com>
2020-02-24 14:22:20 -0500

    animaciones listas

--- src/components/organisms/Home/ProductSection/ProductCard.js
+++ src/components/organisms/Home/ProductSection/ProductCard.js
@@ -1,9 +1,10 @@
 import React from "react";
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import clx from "classnames";
+import { motion } from "framer-motion";
 import "./ProductCard.scss";
 
-export default function ProductCard({ className, image, size, title, price }) {
+export default function ProductCard({ className, image, size, title, price, custom }) {
 
     const sizeDetail = {
         "Tall": "354ml",
@@ -11,16 +12,32 @@ export default function ProductCard({ className, image, size, title, price }) {
         "Venti": "591ml",
     };
 
+    const fadeUp = {
+        initial: {
+            y: 80,
+            opacity: 0,
+            scale: 0.7
+        },
+        animate: i => ({
+            y: 0,
+            opacity: 1,
+            scale: 1,
+            transition: {
+                delay: i * 0.2
+            }
+        })
+    }
+
     return (
-        <span className={clx("product-card-root", className)}>
+        <motion.span variants={fadeUp} initial="initial" animate="animate" custom={custom} whileHover={{ scale: 1.05 }} whileTap={{ scale: 1 }} className={clx("product-card-root", className)}>
             <span className="product-image">
-                <img src={image} alt="Starbucks coffee" />
+                <motion.img initial={{ x: 60, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: (custom + 0.4) * 0.3 }} src={image} alt="Starbucks coffee" />
             </span>
             <span className="product-info">
                 <span className="product-detail">{size} <FiberManualRecordIcon size="small" /> {sizeDetail[size]}</span>
                 <span className="product-title">{title}</span>
                 <span className="product-price">${price}</span>
             </span>
-        </span>
+        </motion.span>
     );
 }
--- src/components/pages/Home/ProductSection/ProductSection.js
+++ src/components/pages/Home/ProductSection/ProductSection.js
@@ -2,15 +2,25 @@ import React from "react";
 import "./ProductSection.scss";
 import ProductCard from "../../../organisms/Home/ProductSection/ProductCard";
 import { productOffers } from "../../../../data/products";
-
+import { motion } from "framer-motion";
 
 export default function ProductSection(){
+
+    const stagger = {
+        animate: {
+            transition: {
+                staggerChildren: 0.3,
+                when: "beforeChildren"
+            }
+        }
+    }
+
     return (
         <section id="productSection">
             <h2 className="product-section-title">Exclusive offers</h2>
-            <div className="products-container">
-                {productOffers.map((product, index) => <ProductCard key={product.id} {...product}/>)}
-            </div>
+            <motion.div variants={stagger} initial={false} animate="animate" className="products-container">
+                {productOffers.map((product, index) => <ProductCard key={product.id} {...product} custom={index}/>)}
+            </motion.div>
         </section>
     );
 }
